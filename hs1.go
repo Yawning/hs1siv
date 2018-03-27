27786e90542b868bb4a8a716296b564a8964a251
Yawning Angel <yawning@schwanenlied.me>
2018-03-27 22:22:43 +0000

    Initial import.
    Works, is timing side channel safe, but is not vectorized.

---
+++ hs1.go
@@ -0,0 +1,122 @@
+// hs1.go - HS1 hash function
+//
+// To the extent possible under law, Yawning Angel has waived all copyright
+// and related or neighboring rights to the software, using the Creative
+// Commons "CC0" public domain dedication. See LICENSE or
+// <http://creativecommons.org/publicdomain/zero/1.0/> for full details.
+
+package hs1siv
+
+import "encoding/binary"
+
+const (
+	hs1NHLen      = 64 // Parameter b
+	hs1HashRounds = 6  // Parameter t
+	hs1SIVLen     = 32 // Parameter l
+
+	m60 = (1 << 60) - 1
+	m61 = (1 << 61) - 1
+
+	hashStateSize = (hs1NHLen/4+4*(hs1HashRounds-1))*4 + hs1HashRounds*8 + hs1HashRounds*3*8
+)
+
+type hs1Ctx struct {
+	nhKey   [hs1NHLen/4 + 4*(hs1HashRounds-1)]uint32
+	polyKey [hs1HashRounds]uint64
+	asuKey  [hs1HashRounds * 3]uint64
+}
+
+func (ctx *hs1Ctx) reset() {
+	burnUint32s(ctx.nhKey[:])
+	burnUint64s(ctx.polyKey[:])
+	burnUint64s(ctx.asuKey[:])
+}
+
+// Return 63 bits congruent to ak+b mod (2^61-1).  Assume 60-bit k,b 63-bit a.
+func polyStep(a, b, k uint64) uint64 {
+	// No uint128_t or equivalent.  Could use inline assembly here, but Go
+	// can't/won't inline it, and the function call overhead will eclipse any
+	// performance gain.
+	m := uint64(uint32(a>>32))*uint64(uint32(k)) + uint64(uint32(k>>32))*uint64(uint32(a))
+	h := uint64(uint32(a>>32)) * uint64(uint32(k>>32))
+	l := uint64(uint32(a)) * uint64(uint32(k))
+	return (l & m61) + (h << 3) + (l >> 61) + b + (m >> 29) + ((m << 32) & m61)
+}
+
+// Reduce a mod (2^61-1) in constant time.
+func polyFinalize(a uint64) uint64 {
+	a = (a & m61) + (a >> 61)
+	mask := uint64(int64((m61-1)-a) >> 63)
+	return a - (mask & m61)
+}
+
+func asuHash(x uint64, k []uint64) uint32 {
+	_ = k[2] // Bounds check elimination.
+	t := k[0] + k[1]*uint64(uint32(x)) + k[2]*uint64(uint32(x>>32))
+	return uint32(t >> 32)
+}
+
+func hashStepRef(ctx *hs1Ctx, in []byte, accum *[hs1HashRounds]uint64) {
+	// len(in) MUST be a multiple of hs1NHLen.
+	inBytes := len(in)
+	for inBytes > 0 {
+		var nhRes [hs1HashRounds]uint64
+		for i := 0; 4*i < hs1NHLen; i += 4 {
+			_ = in[15] // Bounds check elimination.
+			mp0 := binary.LittleEndian.Uint32(in[0:4])
+			mp1 := binary.LittleEndian.Uint32(in[4:8])
+			mp2 := binary.LittleEndian.Uint32(in[8:12])
+			mp3 := binary.LittleEndian.Uint32(in[12:16])
+			for j := 0; j < hs1HashRounds; j += 2 {
+				kp := ctx.nhKey[i+j*4:]
+				_ = kp[7] // Bounds check elimination.
+
+				nhRes[j+0] += uint64(mp0+kp[0]) * uint64(mp2+kp[2])
+				nhRes[j+1] += uint64(mp0+kp[4]) * uint64(mp2+kp[6])
+				nhRes[j+0] += uint64(mp1+kp[1]) * uint64(mp3+kp[3])
+				nhRes[j+1] += uint64(mp1+kp[5]) * uint64(mp3+kp[7])
+			}
+			in = in[16:]
+		}
+		for j := 0; j < hs1HashRounds; j += 2 {
+			accum[j] = polyStep(accum[j], nhRes[j]&m60, ctx.polyKey[j])
+			accum[j+1] = polyStep(accum[j+1], nhRes[j+1]&m60, ctx.polyKey[j+1])
+		}
+
+		inBytes -= hs1NHLen
+	}
+}
+
+func hashFinalizeRef(ctx *hs1Ctx, in []byte, accum *[hs1HashRounds]uint64, result []byte) {
+	inBytes := len(in)
+	if inBytes > 0 {
+		var nhRes [hs1HashRounds]uint64
+		for i := 0; 4*i < inBytes; i += 4 {
+			_ = in[15] // Bounds check elimination.
+			mp0 := binary.LittleEndian.Uint32(in[0:4])
+			mp1 := binary.LittleEndian.Uint32(in[4:8])
+			mp2 := binary.LittleEndian.Uint32(in[8:12])
+			mp3 := binary.LittleEndian.Uint32(in[12:16])
+			in = in[16:]
+			for j := 0; j < hs1HashRounds; j += 2 {
+				kp := ctx.nhKey[i+j*4:]
+				_ = kp[7] // Bounds check elimination.
+
+				nhRes[j+0] += uint64(mp0+kp[0]) * uint64(mp2+kp[2])
+				nhRes[j+1] += uint64(mp0+kp[4]) * uint64(mp2+kp[6])
+				nhRes[j+0] += uint64(mp1+kp[1]) * uint64(mp3+kp[3])
+				nhRes[j+1] += uint64(mp1+kp[5]) * uint64(mp3+kp[7])
+			}
+		}
+		for j := 0; j < hs1HashRounds; j += 2 {
+			accum[j] = polyStep(accum[j], nhRes[j]&m60, ctx.polyKey[j])
+			accum[j+1] = polyStep(accum[j+1], nhRes[j+1]&m60, ctx.polyKey[j+1])
+		}
+	}
+	for j := 0; j < hs1HashRounds; j += 2 {
+		s0 := asuHash(polyFinalize(accum[j]), ctx.asuKey[3*j:])
+		s1 := asuHash(polyFinalize(accum[j+1]), ctx.asuKey[3*j+3:])
+		binary.LittleEndian.PutUint32(result[j*4:], s0)
+		binary.LittleEndian.PutUint32(result[j*4+4:], s1)
+	}
+}
---
+++ hs1siv.go
@@ -0,0 +1,302 @@
+// hs1siv.go - HS1-SIV
+//
+// To the extent possible under law, Yawning Angel has waived all copyright
+// and related or neighboring rights to the software, using the Creative
+// Commons "CC0" public domain dedication. See LICENSE or
+// <http://creativecommons.org/publicdomain/zero/1.0/> for full details.
+
+// Package hs1siv implements the HS1-SIV Authenticated Cipher.
+//
+// While the specification defines multiple parameter sets, this implementation
+// deliberately only supprorts the most conservative "hs1-siv-hi".
+//
+// This implementation is derived from the reference implementation by Ted
+// Krovetz.
+package hs1siv
+
+import (
+	"crypto/subtle"
+	"encoding/binary"
+	"errors"
+)
+
+const (
+	// KeySize is the size of a key in bytes.
+	KeySize = 32
+
+	// NonceSize is the size of a nonce in bytes.
+	NonceSize = 12
+
+	// TagSize is the size of an authentication tag in bytes.
+	TagSize = 32
+
+	stateSize = chachaKeySize + hashStateSize
+)
+
+var (
+	// ErrInvalidKeySize is the error thrown via a panic when a key is an
+	// invalid size.
+	ErrInvalidKeySize = errors.New("hs1siv: invalid key size")
+
+	// ErrInvalidNonceSize is the error thrown via a panic when a nonce is
+	// an invalid size.
+	ErrInvalidNonceSize = errors.New("hs1siv: invalid nonce size")
+
+	// ErrOpen is the error returned when the message authentication fails
+	// during an Open call.
+	ErrOpen = errors.New("hs1siv: message authentication failed")
+
+	settings = [chachaNonceSize]byte{
+		0, 0, hs1SIVLen, 0, chachaRounds, hs1HashRounds, hs1NHLen,
+		0, 0, 0, 0,
+	}
+	zero [hs1SIVLen]byte
+)
+
+// AEAD is a HS1-SIV instance, implementing crypto/cipher.AEAD.
+type AEAD struct {
+	key []byte
+}
+
+// NonceSize returns the size of the nonce that must be passed to Seal and
+// Open.
+func (ae *AEAD) NonceSize() int {
+	return NonceSize
+}
+
+// Overhead returns the maximum difference between the lengths of a plaintext
+// and its ciphertext.
+func (ae *AEAD) Overhead() int {
+	return TagSize
+}
+
+// Seal encrypts and authenticates plaintext, authenticates the
+// additional data and appends the result to dst, returning the updated
+// slice. The nonce must be NonceSize() bytes long and should be unique for
+// all time, for a given key.
+//
+// The plaintext and dst must overlap exactly or not at all. To reuse
+// plaintext's storage for the encrypted output, use plaintext[:0] as dst.
+func (ae *AEAD) Seal(dst, nonce, plaintext, additionalData []byte) []byte {
+	if len(nonce) != NonceSize {
+		panic(ErrInvalidNonceSize)
+	}
+
+	var ctx aeadCtx
+	ctx.setup(ae.key)
+	ret, out := sliceForAppend(dst, len(plaintext)+TagSize)
+	ctx.encrypt(plaintext, additionalData, nonce, out)
+	ctx.reset()
+	return ret
+}
+
+// Open decrypts and authenticates ciphertext, authenticates the
+// additional data and, if successful, appends the resulting plaintext
+// to dst, returning the updated slice. The nonce must be NonceSize()
+// bytes long and both it and the additional data must match the
+// value passed to Seal.
+//
+// The ciphertext and dst must overlap exactly or not at all. To reuse
+// ciphertext's storage for the decrypted output, use ciphertext[:0] as dst.
+//
+// Even if the function fails, the contents of dst, up to its capacity,
+// may be overwritten.
+func (ae *AEAD) Open(dst, nonce, ciphertext, additionalData []byte) ([]byte, error) {
+	var err error
+	var ok bool
+
+	if len(nonce) != NonceSize {
+		panic(ErrInvalidNonceSize)
+	}
+
+	var ctx aeadCtx
+	ctx.setup(ae.key)
+	ret, out := sliceForAppend(dst, len(ciphertext)-TagSize)
+	ok = ctx.decrypt(ciphertext, additionalData, nonce, out)
+	ctx.reset()
+	if !ok {
+		// On decryption failures, purge the invalid plaintext.
+		if len(out) > 0 {
+			burnBytes(out)
+			ret = nil
+		}
+		err = ErrOpen
+	}
+	return ret, err
+}
+
+// Reset securely purges stored sensitive data from the AEAD instance.
+func (ae *AEAD) Reset() {
+	burnBytes(ae.key)
+}
+
+// New returns a new keyed HS1-SIV instance.
+func New(key []byte) *AEAD {
+	if len(key) != KeySize {
+		panic(ErrInvalidKeySize)
+	}
+	return &AEAD{key: append([]byte{}, key...)}
+}
+
+type aeadCtx struct {
+	chachaKey [chachaKeySize]byte
+	hashCtx   hs1Ctx
+
+	sivAccum  [hs1HashRounds]uint64
+	sivLenBuf [16]byte
+}
+
+func (ctx *aeadCtx) reset() {
+	burnBytes(ctx.chachaKey[:])
+	ctx.hashCtx.reset()
+}
+
+// XOR first n bytes of src into dst, then copy the next 32-n bytes.
+func xorCopyChaChaKey(dst, src []byte) {
+	const n = 24 // For 6 hash rounds.
+
+	for i, v := range src[:n] {
+		dst[i] ^= v
+	}
+	copy(dst[n:], src[n:])
+}
+
+func (ctx *aeadCtx) setup(userKey []byte) {
+	// The paper allows a variable length key of up to 256 bits, the reference
+	// implementation hard codes a 128 bit key.
+	//
+	// This implementation only supports a 256 bit key.
+	var chachaNonce [chachaNonceSize]byte
+	copy(chachaNonce[:], settings[:])
+	chachaNonce[0] = byte(len(userKey))
+	var buf [stateSize]byte
+	chacha20(userKey, chachaNonce[:], buf[:], buf[:], 0)
+
+	off := chachaKeySize
+	copy(ctx.chachaKey[:], buf[:off])
+	for i := range ctx.hashCtx.nhKey {
+		ctx.hashCtx.nhKey[i] = binary.LittleEndian.Uint32(buf[off:])
+		off += 4
+	}
+	for i := range ctx.hashCtx.polyKey {
+		ctx.hashCtx.polyKey[i] = binary.LittleEndian.Uint64(buf[off:]) & m60
+		off += 8
+	}
+	for i := range ctx.hashCtx.asuKey {
+		ctx.hashCtx.asuKey[i] = binary.LittleEndian.Uint64(buf[off:])
+		off += 8
+	}
+
+	burnBytes(buf[:])
+}
+
+func (ctx *aeadCtx) sivSetup(aBytes, mBytes int) {
+	// Init: set up lengths, accumulator.
+	binary.LittleEndian.PutUint64(ctx.sivLenBuf[0:8], uint64(aBytes))
+	binary.LittleEndian.PutUint64(ctx.sivLenBuf[8:16], uint64(mBytes))
+	for i := range ctx.sivAccum {
+		ctx.sivAccum[i] = 1
+	}
+}
+
+func (ctx *aeadCtx) sivHashAD(a []byte) {
+	aBytes := len(a)
+
+	// Hash associated data.
+	nhMultiple := aBytes & ^(hs1NHLen - 1)
+	hashStepRef(&ctx.hashCtx, a[:nhMultiple], &ctx.sivAccum)
+	if nhMultiple < aBytes {
+		var buf [hs1NHLen]byte
+		copy(buf[:], a[nhMultiple:])
+		hashStepRef(&ctx.hashCtx, buf[:], &ctx.sivAccum)
+	}
+}
+
+func (ctx *aeadCtx) sivGenerate(m, n, siv []byte) {
+	mBytes := len(m)
+
+	// Hash message data.
+	var chachaKey [chachaKeySize]byte
+	nhMultiple := mBytes & ^(hs1NHLen - 1)
+	hashStepRef(&ctx.hashCtx, m[:nhMultiple], &ctx.sivAccum)
+	mBytes = mBytes - nhMultiple
+	mBytesWithPadding := (mBytes + 15) & ^15
+	if mBytesWithPadding == hs1NHLen {
+		var buf [hs1NHLen]byte
+		copy(buf[:], m[nhMultiple:])
+		hashStepRef(&ctx.hashCtx, buf[:], &ctx.sivAccum)
+		hashFinalizeRef(&ctx.hashCtx, ctx.sivLenBuf[:], &ctx.sivAccum, chachaKey[:])
+	} else {
+		var buf [hs1NHLen]byte
+		copy(buf[:], m[nhMultiple:])
+		copy(buf[mBytesWithPadding:], ctx.sivLenBuf[:])
+		hashFinalizeRef(&ctx.hashCtx, buf[:mBytesWithPadding+16], &ctx.sivAccum, chachaKey[:])
+	}
+
+	// Derive the SIV.
+	xorCopyChaChaKey(chachaKey[:], ctx.chachaKey[:])
+	chacha20(chachaKey[:], n, zero[:], siv, 0)
+
+	burnBytes(chachaKey[:])
+}
+
+func (ctx *aeadCtx) encrypt(m, a, n, c []byte) {
+	mBytes := len(m)
+	var accum [hs1HashRounds]uint64
+	for i := range accum {
+		accum[i] = 1
+	}
+
+	var siv [hs1SIVLen]byte
+	ctx.sivSetup(len(a), len(m))
+	ctx.sivHashAD(a)
+	ctx.sivGenerate(m, n, siv[:])
+
+	var chachaKey [chachaKeySize]byte
+	hashFinalizeRef(&ctx.hashCtx, siv[:], &accum, chachaKey[:])
+	xorCopyChaChaKey(chachaKey[:], ctx.chachaKey[:])
+	chacha20(chachaKey[:], n, m, c, 1)
+	copy(c[mBytes:], siv[:])
+
+	burnBytes(chachaKey[:])
+}
+
+func (ctx *aeadCtx) decrypt(c, a, n, m []byte) bool {
+	cBytes := len(c)
+	if cBytes < hs1SIVLen {
+		return false
+	}
+	mBytes := cBytes - hs1SIVLen
+
+	var accum [hs1HashRounds]uint64
+	for i := range accum {
+		accum[i] = 1
+	}
+
+	var siv, maybeSIV [hs1SIVLen]byte
+	var nonce [NonceSize]byte
+	copy(siv[:], c[mBytes:])
+	copy(nonce[:], n) // Work with a copy, `m` and `n` may alias.
+
+	var chachaKey [chachaKeySize]byte
+	hashFinalizeRef(&ctx.hashCtx, siv[:], &accum, chachaKey[:])
+	xorCopyChaChaKey(chachaKey[:], ctx.chachaKey[:])
+	ctx.sivSetup(len(a), len(m))
+	ctx.sivHashAD(a) // Hash AD before decrption, `m` and `a` may alias.
+	chacha20(chachaKey[:], nonce[:], c[:mBytes], m, 1)
+	ctx.sivGenerate(m, nonce[:], maybeSIV[:])
+
+	return subtle.ConstantTimeCompare(siv[:], maybeSIV[:]) == 1
+}
+
+// Shamelessly stolen from the Go runtime library.
+func sliceForAppend(in []byte, n int) (head, tail []byte) {
+	if total := len(in) + n; cap(in) >= total {
+		head = in[:total]
+	} else {
+		head = make([]byte, total)
+		copy(head, in)
+	}
+	tail = head[len(in):]
+	return
+}
